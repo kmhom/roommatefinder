bdb5ce45f7d2b0129363ddd8bd1cc76c05143f64
Akshay Prabhakaran <a1prabhakaran@scu.edu>
2019-10-26 20:04:45 -0700

    Working on oncampus vc questions

---
+++ SimpleApp/SimpleApp/OnCampusAboutMeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  OnCampusAboutMeViewController.swift
+//  SimpleApp
+//
+//  Created by akshay on 10/25/19.
+//  Copyright © 2019 Kevin Hom. All rights reserved.
+//
+
+import UIKit
+
+
+
+class OnCampusAboutMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view.
+    }
+
+}
