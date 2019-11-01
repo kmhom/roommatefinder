8fdf01f2e59756127bb7aed11b5b6e528ca348dc
Akshay Prabhakaran <a1prabhakaran@scu.edu>
2019-10-31 18:31:28 -0700

    On campus viewcontroller added

---
+++ SimpleApp/SimpleApp/OffCampusViewController.swift
@@ -0,0 +1,43 @@
+//
+//  OffCampusViewController.swift
+//  SimpleApp
+//
+//  Created by akshay on 10/30/19.
+//  Copyright © 2019 Kevin Hom. All rights reserved.
+//
+
+import UIKit
+
+class OffCampusViewController: UIViewController {
+
+    @IBOutlet weak var carPickerField: UIPickerView!
+    @IBOutlet weak var accomodationPickerField: UIPickerView!
+    @IBOutlet weak var additionalinfoTextField: UITextView!
+    @IBOutlet weak var guestPickerField: UIPickerView!
+    @IBOutlet weak var numGuestPickerField: UIPickerView!
+    @IBOutlet weak var sharesstuffPickerField: UIPickerView!
+    @IBOutlet weak var messyorgPickerField: UIPickerView!
+    @IBOutlet weak var ispetPickerField: UIPickerView!
+    @IBOutlet weak var additionalinfoTextView: UITextView!
+    
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
